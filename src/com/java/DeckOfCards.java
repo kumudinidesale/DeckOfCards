e1fc8c72c5aba97453a37c17230b04c777c646e8
kumudinidesale <kdesale1211@gmail.com>
2022-03-07 08:47:41 +0530

    added:Setup the initial game with deck of cards

--- src/com/java/DeckOfCards.java
+++ src/com/java/DeckOfCards.java
@@ -1,7 +1,49 @@
 package com.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DeckOfCards {
-	public static void main(String[] args) {
+	/*
+	 * @purpose: Ability to check Cards and sure we have unique cards.
+	 * 
+	 * 
+	 * 
+	 * @return: ranks to further compare.
+	 */
+
+	public static ArrayList<String> deckCards = new ArrayList<>();
+	public static Scanner scanner = new Scanner(System.in);
+
+	public void wellCome() {
 		System.out.println("Welcome to the Deck of Cards DashBoard");
+	}
+
+	public void deckOfCard() {
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
+		int numOfCards = suits.length * ranks.length;
+		System.out.println("Number of cards in the deck:" + numOfCards);
+		for (int i = 0; i < ranks.length; i++) {
+			for (int j = 0; j < suits.length; j++) {
+				deckCards.add(ranks[i] + "->" + suits[j]);
+			}
+		}
+		toDisplay(deckCards);
+	}
+
+	public static void toDisplay(ArrayList<String> cardsDeck) {
+		System.out.println("\nCards in Deck:");
+		for (String element : cardsDeck) {
+			System.out.println(element);
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.wellCome();
+		deckOfCardsGame.deckOfCard();
+
 	}
 }
